e2115faa0d1310ebf656929a435059397bee2c74
dishadutta <duttadisha12345@gmail.com>
2021-07-06 15:51:07 +0530

    Updates

---
+++ precache-manifest.0660ce63773654c1d44b39780b862313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0838403626e2c48ba22cf50140458673",
+    "url": "/CalculatorApp/index.html"
+  },
+  {
+    "revision": "fbb1d1524af5be9e8a3e",
+    "url": "/CalculatorApp/static/css/main.5a8f529d.chunk.css"
+  },
+  {
+    "revision": "2bd7e858bae140335cae",
+    "url": "/CalculatorApp/static/js/2.b3a57b3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CalculatorApp/static/js/2.b3a57b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb1d1524af5be9e8a3e",
+    "url": "/CalculatorApp/static/js/main.c4836725.chunk.js"
+  },
+  {
+    "revision": "80419c34d5e0343fadab",
+    "url": "/CalculatorApp/static/js/runtime-main.efbc2e06.js"
+  }
+]);
